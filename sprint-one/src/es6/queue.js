15b79e87ef6fcbb389952d09cf5597b77b6a7580
khengschew <khengc@gmail.com>
2019-01-04 15:50:16 -0800

    ES6: Implemented stack and queue using the Class keyword and constructor

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[this.size()] = value;
+  }
+
+  dequeue() {
+    var tempVal = this.storage[0];
+    var tempSize = this.size();
+
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    delete this.storage[tempSize - 1];
+
+    return tempVal;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
