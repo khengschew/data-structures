951ee88f42a467e73e1511d7d5eb6b282be6f1ff
khengschew <khengc@gmail.com>
2019-01-04 12:22:11 -0800

    functional-queue: Implemented .enqueue, .dequeue, and .size methods for functional queue class

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,31 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // Add value to storage at index someInstance.size
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    // Retrieve and store value at index 0 in storage
+    // Remove value at 0 index from storage
+    // Re-name/number the indices for remaining storage items
+    // Remove value at last index from storage
+    // Return retrieved stored value at index 0
+    var tempRet = storage[0];
+    var tempSize = someInstance.size();
+
+    delete storage[0];
+    for (var key in storage) {
+      storage[key - 1] = storage[key];
+    }
+    delete storage[tempSize - 1];
+
+    return tempRet;
   };
 
   someInstance.size = function() {
+    // Return the length of storage object keys
+    return Object.keys(storage).length;
   };
 
   return someInstance;
