bc66db2bc17212df9906d27caa4b4f34941eb952
khengschew <khengc@gmail.com>
2019-01-04 11:44:29 -0800

    functional-stack: added pseudocode for functional instantiation stack methods

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,20 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Get the last index in storage
+    // Add storage[last index + 1] = value
   };
 
   someInstance.pop = function() {
+    // Get the last index in storage
+    // Get and store the last index value in storage
+    // Delete last index and value from storage
+    // Return stored last index value
   };
 
   someInstance.size = function() {
+    // Get the last index in storage
+    // Return the last index in storage
   };
 
   return someInstance;
