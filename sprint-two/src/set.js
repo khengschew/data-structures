7b3d9a46be24b79dd2de1927598fe085522e9dfd
khengschew <khengc@gmail.com>
2019-01-07 16:39:42 -0800

    Implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var storedItem of this._storage) {
+    if (storedItem === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  if (index !== -1) {
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
